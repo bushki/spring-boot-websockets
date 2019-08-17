b22e77b2775dc6b137c432a32e2d4d7bf09f0d82
Luis <luis.p.melgar@disney.com>
2019-08-17 13:48:28 -0700

    fixed null object error, added simple html view

--- src/main/java/com/example/springbootwebsocket/config/SchedulerConfig.java
+++ src/main/java/com/example/springbootwebsocket/config/SchedulerConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootwebsocket.config;
 
 import com.example.springbootwebsocket.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 public class SchedulerConfig {
 
+    @Autowired
     SimpMessagingTemplate template;
 
     @Scheduled(fixedDelay = 5000)
