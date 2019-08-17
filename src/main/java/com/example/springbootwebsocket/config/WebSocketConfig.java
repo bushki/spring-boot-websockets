a7d85bee203b7e9a8325d0167a7b256065a01c8f
Luis <luis.p.melgar@disney.com>
2019-08-17 12:36:57 -0700

    added config to enable message broker
    note that I switched to WebSocketMessageBrokerConfigurer

---
+++ src/main/java/com/example/springbootwebsocket/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.example.springbootwebsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        /*
+        This will enable Spring’s STOMP support and SockJS fallback options,
+        so that alternative messaging options may be used if WebSockets are not available.
+         */
+        registry.addEndpoint("/websocket-demo").withSockJS();
+    }
+
+}
