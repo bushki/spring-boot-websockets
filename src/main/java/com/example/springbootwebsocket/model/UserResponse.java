e7357f9deb0ad79073a919ebf154ef15cb9db504
Luis <luis.p.melgar@disney.com>
2019-08-17 12:22:51 -0700

    initial commit

---
+++ src/main/java/com/example/springbootwebsocket/model/UserResponse.java
@@ -0,0 +1,17 @@
+package com.example.springbootwebsocket.model;
+
+public class UserResponse {
+    public UserResponse(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+}
